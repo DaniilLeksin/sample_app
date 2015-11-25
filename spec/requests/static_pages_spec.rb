acae331674dd64a9573be6d2cf3124c22e26a804
dleksin <danilaleksin@gmail.com>
2015-11-25 10:23:26 +0300

    path3 task1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -34,9 +34,21 @@ describe "StaticPages" do
       expect(page).to have_content('About Us')
     end
 
-    it "should have the content 'About Us'" do
+    it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_content('About Us')
+      expect(page).to have_title('About Us')
     end
+  end
+
+  describe "Contact page" do
+  	it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+  	end
+
+  	it "should have title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Contact')
+  	end
   end
 end
